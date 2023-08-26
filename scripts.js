ce3373744dc76b28cbd90cb76e1ad2a982124a38
Ryan Mason <rtm248@nau.edu>
2023-08-25 23:36:13 -0700

    - Added local Jquery file of version 3.6.3 - Implemented basic Time Zone Display (not desired format)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 
+//ensure all resources in document are ready to be displayed
 $(document).ready(function() {
+
+// function that will execute when convertBtn(button id) in html is pressed
 $('#convertBtn').click(function() {
+
+    // constant for address
     const address = $('#address').val();
+
+    // Ensure address entry is not blank
     if (address === '') {
-    $('#result').text('Please enter an address.');
+
+    $('#result').text('Please Enter an Address');
     return;
     }
 
-    const mapsApiKey = 'AIzaSyBjQuWkOeXIkD9AfKSaGZIxUjtXs60t6dQ';
+    const mapsApiKey = 'AIzaSyA6LXL7L2bzcy1evbjJha4E8RB_7K0t7rs';
 
     //Convert address to coordinates using Geocoding API
     const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${mapsApiKey}`;
@@ -21,14 +29,23 @@ $('#convertBtn').click(function() {
         //Get time zone using Time Zone API
         const timezoneUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now() / 1000)}&key=${mapsApiKey}`;
 
+
         $.get(timezoneUrl, function(timezoneData) {
         const timeZoneId = timezoneData.timeZoneId;
         const timeZoneName = timezoneData.timeZoneName;
 
+        
+        var rawOffset = new Date();
+        const convertOffset = rawOffset / 4;
+
+        $('#result').html(`Raw Offset: ${rawOffset} Converted Offset: ${convertOffset}`); //TESTING
+
+        $('#result').html(`Time Zone ID: ${timeZoneId}<br>Time Zone Name: ${timeZoneName}`);
+
         $('#result').html(`Time Zone ID: ${timeZoneId}<br>Time Zone Name: ${timeZoneName}`);
         });
     } else {
-        $('#result').text('Geocoding API request failed. Please check your address or API key.');
+        $('#result').text('Error Retrieving Time Zone');
     }
     });
 });
